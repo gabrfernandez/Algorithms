7ba764e7c3510491e9cb222d1c0dbd529f0fb85f
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2021-11-15 21:12:17 -0800

    added top 25 file

---
+++ Algorithms/Algoriths-25top.js
@@ -0,0 +1,59 @@
+//top 25 algorithms
+
+//searching 
+//linear search
+
+//binary search
+
+// DFS
+
+//BFS
+
+//Sorting 
+//Insertion sort
+
+//Heap Sort 
+
+//Selection Sort
+
+//Merge Sort
+
+//Quick Sort
+
+//Counting Sort
+
+//Graphs
+//Kruskal's 
+
+//Dijkstra's 
+
+//Bellman Ford
+
+//Floyd Warshall
+
+//Topological Sort
+
+//Flood Fill
+
+//Lee
+
+//Arrays
+//Kadane's 
+
+//Floyd's Cycle Detection 
+
+//KMP
+
+//Quick Sort
+
+//Boyer-More Majority Vote 
+
+//Basics
+//Huffman Coding Compression
+
+//Euclid's 
+
+//Union Find
+
+
+
