ecd163d0117e5fd8b0f367505e8a93fb5baad7e2
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2022-01-17 22:48:09 -0800

    generate parenthesis

--- Algorithms/Algorithms-10common.js
+++ Algorithms/Algorithms-10common.js
@@ -151,3 +151,39 @@ const areSymmetric=(left, right)=> {
 isSymmetric([1,2,2,3,4,4,3]) //output:true
 //T(n)=O(n) S(n)=O(logn)
 
+//Generate parentheses
+//backtracking problem
+//given an integer n, generate all the valid combos of n pairs of parentheses
+const generateParenthesis = (n)=> {
+    const combinations = [];
+    // Here we know we can only start with an opening bracket => use 1 open:
+    // but we can also start with an empty string and 0 like so:
+    // buildCombinationFrom('', 0, 0, n, combinations);
+    buildCombinationFrom('(', 1, 0, n, combinations);
+    return combinations;
+};
+
+const buildCombinationFrom=(string, openUsed, closeUsed, n, combinations)=> {
+    // Base case: when we reach 2n length
+    if (string.length === 2*n) {
+        // Add the string to the list of combination:
+        combinations.push(string);
+        // Exit from this recursive call.
+        return;
+    }
+    // Case: when we can add more opening bracket:
+    // If we haven't used all opening bracket (n pairs = n opens)
+    if (openUsed < n) {
+        // Add 1 opening, update opening used:
+        buildCombinationFrom(string + '(', openUsed + 1, closeUsed, n, combinations);
+    }
+    // Case: when we can add more closing bracket:
+    // If we have more opening than closing:
+    if (openUsed > closeUsed) {
+        // Add 1 closing, update closing used:
+        buildCombinationFrom(string + ')', openUsed, closeUsed + 1, n, combinations);
+    }
+}
+generateParenthesis(3) // output: ["((()))","(()())","(())()","()(())","()()()"]
+//Time = O(4^n/(sqrt(n))) Space = O(4^n/(sqrt(n)))
+
